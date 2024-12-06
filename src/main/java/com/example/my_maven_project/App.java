3e54ddbbccb5493628871c78f5f5ddb222a1f4d6
Chaitanya16050 <chaitanyagattu7@gmail.com>
2024-12-06 10:22:32 +0530

    Update App.java

--- src/main/java/com/example/my_maven_project/App.java
+++ src/main/java/com/example/my_maven_project/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("Changes made");
+        System.out.println("Changes made1");
     }
 }
